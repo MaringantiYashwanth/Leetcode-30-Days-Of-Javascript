aaf951604d14dca4f0faae3b2c96b290943c3b80
MaringantiYashwanth <51596895+MaringantiYashwanth@users.noreply.github.com>
2023-06-03 10:37:23 +0530

    updating 30 days of python

---
+++ Day 2/NumbersVariablesAndStrings.py
@@ -0,0 +1,17 @@
+print(12 * 31)
+print(24 / 432)
+print("3" * 232)
+print('hello world' * 3)
+phrase = "and that's what we want to see"
+number = 123123
+numb2 = "12312"
+print(number * 3129012)
+print(numb2 * 3)
+# print("hello world this is a "quote" awesome time") 
+# this will not work
+doublequote_greeting = "hello world this is a \"quote\" awesome time"
+print(doublequote_greeting)
+singlequote_greeting = 'hello world this is a \'quote\' here'
+print(singlequote_greeting)
+print('hello world "quote" inside')
+print("hello\nworld")
